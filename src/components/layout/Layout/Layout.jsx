7f7810939b5572fb81e2e0e283255ee5c67fcd0f
Evan Salzbrenner <hello@aboutevan.com>
2016-11-28 13:56:45 -0800

    header

--- src/components/layout/Layout/Layout.jsx
+++ src/components/layout/Layout/Layout.jsx
@@ -1,9 +1,19 @@
-import template from './Layout.template';
+import React from 'react';
+import Header from 'presentation/Header/Header';
 
 if (process.env.BROWSER) {
   require('./Layout.sass');
 }
 
-const Layout = template;
+const Layout = props => (
+  <div className="layout">
+    <Header />
+    <div className="layout__content">{props.children}</div>
+  </div>
+);
+
+Layout.propTypes = {
+  children: React.PropTypes.node.isRequired,
+};
 
 export default Layout;
--- src/components/presentation/Header/Header.jsx
+++ src/components/presentation/Header/Header.jsx
@@ -1,3 +1,11 @@
 import React from 'react';
 
-const Header
+const Header = () => (
+  <div className="header">
+    <h1>
+      Im a header
+    </h1>
+  </div>
+);
+
+export default Header;
