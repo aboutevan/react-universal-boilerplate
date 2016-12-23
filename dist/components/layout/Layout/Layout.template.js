82da5233d706638eadaa38c95c8e61ecbe9117e1
Evan Salzbrenner <hello@aboutevan.com>
2016-12-22 17:21:49 -0800

    testing herouku

---
+++ dist/components/layout/Layout/Layout.template.js
@@ -0,0 +1,29 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var template = function template(props) {
+  return _react2.default.createElement(
+    "div",
+    { className: "layout" },
+    _react2.default.createElement(
+      "div",
+      { className: "layout__content" },
+      props.children
+    )
+  );
+};
+
+template.propTypes = {
+  children: _react2.default.PropTypes.node.isRequired
+};
+
+exports.default = template;
---
+++ dist/components/page/IndexPage/IndexPage.template.js
@@ -0,0 +1,25 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var template = function template() {
+  return _react2.default.createElement(
+    "div",
+    { className: "index-page" },
+    _react2.default.createElement(
+      "div",
+      null,
+      "Index Page"
+    )
+  );
+};
+
+exports.default = template;
