78eba9dc02b8d03ec65637f705115e1ea0b2b730
Evan Salzbrenner <hello@aboutevan.com>
2016-12-22 18:59:39 -0800

    update

---
+++ dist/components/presentation/Header/Header.js
@@ -0,0 +1,25 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var Header = function Header() {
+  return _react2.default.createElement(
+    "div",
+    { className: "header" },
+    _react2.default.createElement(
+      "h1",
+      null,
+      "Im a header"
+    )
+  );
+};
+
+exports.default = Header;
---
+++ dist/init.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactDom = require('react-dom');
+
+var _reactDom2 = _interopRequireDefault(_reactDom);
+
+var _App = require('./components/App');
+
+var _App2 = _interopRequireDefault(_App);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// $(document).foundation();
+_reactDom2.default.render(_react2.default.createElement(_App2.default, null), document.getElementById('main'));
+
+if (process.env.NODE_ENV === 'development') {
+  module.hot.accept('./components/App', function () {
+    var NewApp = require('./components/App').default;
+    _reactDom2.default.render(_react2.default.createElement(NewApp, null), document.getElementById('main'));
+  });
+}
