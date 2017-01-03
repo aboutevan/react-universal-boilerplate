7edd2c9d8e1a2cfccc07ebc556d6b5a85e0a3728
Evan Salzbrenner <hello@aboutevan.com>
2017-01-03 09:27:52 -0800

    add pixi container

--- src/components/presentation/HeroCanvas/HeroCanvas.jsx
+++ src/components/presentation/HeroCanvas/HeroCanvas.jsx
@@ -2,41 +2,49 @@ import React, { Component } from 'react';
 // import scaleToWindow from 'utils/scaleToWindow.utils';
 
 export default class HeroCanvas extends Component {
-  // constructor(props) {
-  //   super(props);
-  // }
-  componentDidMount() {
-    // scripts called in componentDidMount will not execute
-    // until after component is rendered. This means we can
-    // use scripts meant for use in browser in our server-side
-    // apps - the scripts won't be passed to our Node server and
-    // result in undefined errors - not the greatest solution, but
-    // all I could find at this time
-    const PIXI = require('pixi.js');
-    this.pixi = PIXI;
+  constructor(props) {
+    super(props);
 
-    // create renderer
-    this.renderer = PIXI.autoDetectRenderer(document.body.clientWidth, window.innerHeight);
-
-    // append it to component
-    this.component.appendChild(this.renderer.view);
-    // scaleToWindow(this.renderer.view);
-    window.addEventListener('resize', () => {
-      this.renderer.view.width = document.body.clientWidth;
-      // this.renderer.view.height = document.body.clientHeight;
-      // scaleToWindow(this.renderer.view);
-    });
-    this.stage = new PIXI.Container();
-    this.pixiAddItemstoLoader();
-    this.pixiLoadLoader();
-
-    this.pixiRender();
-
+    // dispose of appended elements while hot reloading
     if (module.hot) {
       module.hot.dispose(() => {
         this.component.removeChild(this.renderer.view);
       });
     }
+  }
+  componentDidMount() {
+    // scripts called in componentDidMount will not execute
+    // until after component is rendered. This means we can
+    // use scripts meant for use in browser in our server-side
+    // apps - the scripts won't be passed to our Node server and
+    // result in undefined errors - not the greatest solution
+    const PIXI = require('pixi.js');
+
+    PIXI.utils.skipHello(); // disable pixi banner in console
+    this.pixi = PIXI;
+
+    // create renderer
+    this.renderer = PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight);
+    const canvas = this.renderer.view;
+    // scaleToWindow(this.renderer.view);
+
+    // append it to component
+    this.component.appendChild(canvas);
+
+    this.stage = new PIXI.Container();
+    this.spriteContainer = new PIXI.Container();
+    this.stage.addChild(this.spriteContainer);
+
+    this.pixiAddItemstoLoader();
+    this.pixiLoadLoader();
+
+    this.pixiRender();
+
+    // resize events
+    window.addEventListener('resize', () => {
+      this.renderer.resize(window.innerWidth, window.innerHeight);
+      // scaleToWindow(this.renderer.view);
+    });
   }
   pixiAddItemstoLoader() {
     const PIXI = this.pixi;
@@ -56,18 +64,25 @@ export default class HeroCanvas extends Component {
   }
   pixiStaging() {
     const PIXI = this.pixi;
-    let i = 0;
+    const canvasW = this.renderer.view.width;
+    const canvasH = this.renderer.view.height;
+    // let i = 0;
     const texture = PIXI.loader.resources['../img/M.png'].texture;
-    while (i < 10) {
-      i += 1;
-      const sprite = new PIXI.Sprite(texture);
-      sprite.x = 64 + i;
-      sprite.y = 135 + i;
-      this.stage.addChild(sprite);
-      // console.log(sprite);
-    }
-    const canvas = this.component.firstChild;
-    console.log(canvas);
+    const sprite = new PIXI.Sprite(texture);
+    this.spriteContainer.addChild(sprite);
+    sprite.x = canvasW * 0.1;
+    sprite.y = canvasH * 0.5;
+    sprite.scale.x = 0.5;
+    sprite.scale.y = 0.5;
+    // sprite.y = '64%';
+    // while (i < 10) {
+    //   i += 1;
+    //   const sprite = new PIXI.Sprite(texture);
+    //   sprite.x = 64 + i;
+    //   sprite.y = 135 + i;
+    //   this.stage.addChild(sprite);
+    //   // console.log(sprite);
+    // }
     // console.log(this.renderer.plugins);
     // this.renderer.plugins.interaction.onMouseOver(() => {
     //   console.log('im over');
